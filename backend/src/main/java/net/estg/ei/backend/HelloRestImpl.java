162b1187787b25247edfda83def722dc94e9431f
André Colaço Alves Filho <2212948@my.ipleiria.pt>
2024-05-24 21:28:04 +0100

    setup backend

---
+++ backend/src/main/java/net/estg/ei/backend/HelloRestImpl.java
@@ -0,0 +1,13 @@
+package net.estg.ei.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestImpl
+{
+  @GetMapping("/")
+  public String index() {
+    return "Greetings from Spring Boot!";
+  }
+}
