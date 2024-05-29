01cc17803e61954d09cca715f739eadb22f701c7
Dora Mendes <dora.mendes@voidsoftware.com>
2024-05-29 02:09:44 +0100

    binary packets processed: empty values being filled with average (from datasets), or -1 or 2 according to the Merged.ipynb file

--- Dora/PACKETS_Final/Binario/1.py
+++ Dora/PACKETS_Final/Binario/1.py
@@ -40,8 +40,6 @@ new_model = tf.keras.models.load_model('model.h5', compile = False)
 # tcp.flags.urg
 # tcp.urgent_pointer
 # ip.frag_offset
-# is_malicious
-# attack_type
 # eth.dst.ig
 # eth.src.ig,
 # eth.src.lg
@@ -60,50 +58,95 @@ def is_ns_flag_set(tcp_flags):
     # Check if the 9th bit is set
     return tcp_flags_int & 0x0100 != 0
 
+# retornar valores de cada campo
+def getFieldValue(res, field, default):
+    if field in res:
+        return float(res[field])
+    else: return float(default)
+
+# valores médios de cada campo
+def getAverageValue(field):
+    res = {
+        'http_http_content_length': 0.06,
+        'http_http_response_code': 0.03,
+        'http_http_response_number': 0.08,
+        'http_http_time': 0.01,
+        'tcp_tcp_analysis_initial_rtt': 0.00,
+        'tcp_tcp_completeness_fin': 0.07,
+        'tcp_tcp_completeness_syn': 0.09,
+        'tcp_tcp_completeness_syn-ack': 0.03,
+        'tcp_tcp_flags_cwr': 0.09,
+
+        'http.request': 0.08,
+        'http.response': 0.08,
+        'tcp.analysis.bytes_in_flight': 0.12,
+        'tcp.analysis.ack_rtt': 0.00,
+        'tcp.analysis.push_bytes_sent': 0.12,
+        'tcp_tcp_flags_ece': 0.09,
+        'tcp_tcp_flags_fin': -0.03,
+        'tcp_tcp_flags': -0.01,
+        'tcp_tcp_flags_res': 0.00,
+        'tcp_tcp_flags_syn': 0.03,
+        'tcp_tcp_flags_urg': 0.09,
+        'tcp_tcp_urgent_pointer': 0.09,
+
+        'ip_ip_frag_offset': 0.19,
+        
+        'eth_eth_dst_ig': 0.50,
+        'eth_eth_src_ig': 0.50,
+        'eth_eth_src_lg': 0.50,
+        'eth.src_not_group': 0.50,
+        
+        'arp.isannouncement': 0.00,
+    }
+    return res[field];
+
 # for para percorrer todos os jsons (1 json = 1 packet)
 for x in iter(tsharkProcess.stdout.readline, ""):
     y = json.loads(x)
     if 'layers' in y and 'tcp' in y['layers'] and 'http' in y['layers'] and 'http_http_content_length' in y['layers']['http']:
 
-        # descobri o tipo de tcp_tcp_flags
+        # descobrir o tipo do campo tcp_tcp_flags
         # print(type(y['layers']['tcp']['tcp_tcp_flags']));
-        print([y['layers']['tcp']['tcp_tcp_analysis_initial_rtt']])
 
-        # 25 campos
+        # descobrir o valor do campo tcp_tcp_analysis_initial_rtt
+        # print([y['layers']['tcp']['tcp_tcp_analysis_initial_rtt']])
+        print([y['layers']['eth']['eth_eth_src_lg']])
+        # print([y['layers']['eth']['eth_eth_src_not_group']])
+        print([y['layers']['arp']['arp_arp_isannouncement']])
+
+        # 23 campos
         d = {
-            "http.content_length": [float(y['layers']['http']['http_http_content_length'] or 0.5)], # average
-            "http.request": [0.0], # -1
-            "http.response.code": [float(y['layers']['http']['http_http_response_code'])], # average
-            "http.response_number":  [float(y['layers']['http']['http_http_response_number'])], # -1
-            "http.time": [float(y['layers']['http']['http_http_time'])], # average
+            "http.content_length": [getFieldValue(y['layers']['http'], 'http_http_content_length', getAverageValue('http_http_content_length'))],
+            "http.request": [-1.0],
+            "http.response.code": [getFieldValue(y['layers']['http'], 'http_http_response_code', getAverageValue('http_http_response_code'))],
+            "http.response_number":  [getFieldValue(y['layers']['http'], 'http_http_response_number', -1)],
+            "http.time": [getFieldValue(y['layers']['http'], 'http_http_time', getAverageValue('http_http_time'))],
 
-            "tcp.analysis.initial_rtt": [float(y['layers']['tcp']['tcp_tcp_analysis_initial_rtt'])],  # average
-            "tcp.connection.fin": [float(y['layers']['tcp']['tcp_tcp_completeness_fin'])], # -1
-            "tcp.connection.syn": [float(y['layers']['tcp']['tcp_tcp_completeness_syn'])], # -1
-            "tcp.connection.synack": [float(y['layers']['tcp']['tcp_tcp_completeness_syn-ack'])], # -1
+            "tcp.analysis.initial_rtt": [getFieldValue(y['layers']['tcp'], 'tcp_tcp_analysis_initial_rtt', getAverageValue('tcp_tcp_analysis_initial_rtt'))],
+            "tcp.connection.fin": [getFieldValue(y['layers']['tcp'], 'tcp_tcp_completeness_fin', -1)],
+            "tcp.connection.syn": [getFieldValue(y['layers']['tcp'], 'tcp_tcp_completeness_syn', -1)],
+            "tcp.connection.synack": [getFieldValue(y['layers']['tcp'], 'tcp_tcp_completeness_syn-ack', -1)],
 
-            "tcp.flags.cwr": [float(y['layers']['tcp']['tcp_tcp_flags_cwr'])], # -1
-            "tcp.flags.ecn": [float(y['layers']['tcp']['tcp_tcp_flags_ece'])], # -1
-            "tcp.flags.fin": [float(y['layers']['tcp']['tcp_tcp_flags_fin'])], # +2
-            "tcp.flags.ns": [1.0 if is_ns_flag_set(y['layers']['tcp']['tcp_tcp_flags']) else 0.0], # -1
-            "tcp.flags.res": [float(y['layers']['tcp']['tcp_tcp_flags_res'])], # -1
-            "tcp.flags.syn": [float(y['layers']['tcp']['tcp_tcp_flags_syn'])], # +2
-            "tcp.flags.urg": [float(y['layers']['tcp']['tcp_tcp_flags_urg'])], # -1
-            "tcp.urgent_pointer": [norm.cdf([float(y['layers']['tcp']['tcp_tcp_urgent_pointer'])])], # +2
+            "tcp.flags.cwr": [getFieldValue(y['layers']['tcp'], 'tcp_tcp_flags_cwr', -1)],
+            "tcp.flags.ecn": [getFieldValue(y['layers']['tcp'], 'tcp_tcp_flags_ece', -1)], 
+            "tcp.flags.fin": [getFieldValue(y['layers']['tcp'], 'tcp_tcp_flags_fin', 2)], 
+            "tcp.flags.ns": [1.0 if is_ns_flag_set(y['layers']['tcp']['tcp_tcp_flags']) else 0.0], #todo -1
+            "tcp.flags.res": [getFieldValue(y['layers']['tcp'], 'tcp_tcp_flags_res', -1)], 
+            "tcp.flags.syn": [getFieldValue(y['layers']['tcp'], 'tcp_tcp_flags_syn', 2)],
+            "tcp.flags.urg": [getFieldValue(y['layers']['tcp'], 'tcp_tcp_flags_urg', -1)],
+            "tcp.urgent_pointer": [norm.cdf([float(y['layers']['tcp']['tcp_tcp_urgent_pointer'])])], #todo 2 e zscore
 
-            "ip.frag_offset": [float(y['layers']['ip']['ip_ip_frag_offset'])], # -1
+            "ip.frag_offset":  [getFieldValue(y['layers']['ip'], 'ip_ip_frag_offset', -1)],
 
-            # modelo não precisa destes campos... eles sao os outputs do modelo
-            # "is_malicious": [0.0],
-            # "attack_type": [0.0],
+            "eth.dst.ig": [getFieldValue(y['layers']['eth'], 'eth_eth_dst_ig', -1)],
+            "eth.src.ig": [getFieldValue(y['layers']['eth'], 'eth_eth_src_ig', -1)], 
+            "eth.src.lg": [getFieldValue(y['layers']['eth'], 'eth_eth_src_lg', -1)],
+            "eth.src_not_group": [-1.0], #todo -1 
 
-            "eth.dst.ig": [float(y['layers']['eth']['eth_eth_dst_ig'])], # -1
-            "eth.src.ig": [float(y['layers']['eth']['eth_eth_src_ig'])], # -1
-            "eth.src.lg": [float(y['layers']['eth']['eth_eth_src_lg'])], # -1
-            "eth.src_not_group": [1.0], # -1
-
-            "arp.isannouncement": [0.0], # -1
+            "arp.isannouncement": [-1.0], #todo -1
         }
+
         df = pd.DataFrame(data=d)
 
         # pre-proc. supostamente falta aqui
