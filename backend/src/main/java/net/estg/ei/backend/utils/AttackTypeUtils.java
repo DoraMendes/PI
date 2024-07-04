7980f859d3510766d8c4f73c85920c567d82fd88
Dora Mendes <doramendes98@hotmail.com>
2024-07-04 23:23:43 +0100

    changed attack type mapping

--- backend/src/main/java/net/estg/ei/backend/utils/AttackTypeUtils.java
+++ backend/src/main/java/net/estg/ei/backend/utils/AttackTypeUtils.java
@@ -1,22 +1,23 @@
 package net.estg.ei.backend.utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import net.estg.ei.backend.enums.AttackType;
 
 public class AttackTypeUtils {
+    private static ArrayList<AttackType> attackTypeDoubleListMapping = new ArrayList<AttackType>(
+        Arrays.asList( AttackType.APACHE_KILLER, AttackType.RUDY, AttackType.SLOW_READ,
+        AttackType.SLOW_LORIS, AttackType.ARP_SPOOFING, AttackType.CAM_OVERFLOW,
+        AttackType.MQTT_MALARIA, AttackType.NETWORK_SCAN,)
+    );
 
     public static AttackType getAttackType(List<Double> predictions) {
-        return switch (1) {
-            case predictions.get(1).intValue() -> AttackType.APACHE_KILLER;
-            case predictions.get(2).intValue() -> AttackType.RUDY;
-            case predictions.get(3).intValue() -> AttackType.SLOW_READ;
-            case predictions.get(4).intValue() -> AttackType.SLOW_LORIS;
-            case predictions.get(5).intValue() -> AttackType.ARP_SPOOFING;
-            case predictions.get(6).intValue() -> AttackType.CAM_OVERFLOW;
-            case predictions.get(7).intValue() -> AttackType.MQTT_MALARIA;
-            case predictions.get(8).intValue() -> AttackType.NETWORK_SCAN;
-            default -> AttackType.UNKNOWN;
+        for (int i = 0; i < predictions.size(); i++) {
+            if (predictions.get(i) == 1.0) return attackTypeDoubleListMapping.get(i); 
         }
+        
+        return AttackType.UNKNOWN;
     } 
 }
