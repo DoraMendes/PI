2bd9d67656a0d80e86b0803bb8876401fe4eba8e
Dora Mendes <doramendes98@hotmail.com>
2024-07-05 00:29:55 +0100

    fixed attacktype extra comman

--- backend/src/main/java/net/estg/ei/backend/utils/AttackTypeUtils.java
+++ backend/src/main/java/net/estg/ei/backend/utils/AttackTypeUtils.java
@@ -7,10 +7,10 @@ import java.util.List;
 import net.estg.ei.backend.enums.AttackType;
 
 public class AttackTypeUtils {
-    private static ArrayList<AttackType> attackTypeDoubleListMapping = new ArrayList<AttackType>(
+    private static ArrayList<AttackType> attackTypeDoubleListMapping = new ArrayList<>(
         Arrays.asList( AttackType.APACHE_KILLER, AttackType.RUDY, AttackType.SLOW_READ,
         AttackType.SLOW_LORIS, AttackType.ARP_SPOOFING, AttackType.CAM_OVERFLOW,
-        AttackType.MQTT_MALARIA, AttackType.NETWORK_SCAN,)
+        AttackType.MQTT_MALARIA, AttackType.NETWORK_SCAN)
     );
 
     public static AttackType getAttackType(List<Double> predictions) {
