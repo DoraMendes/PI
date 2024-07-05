61d948440d35f8a716f80da75e3fbcc6fccff7f5
Dora Mendes <doramendes98@hotmail.com>
2024-07-05 12:21:30 +0100

    removed attack type normal from fori iteration

--- backend/src/main/java/net/estg/ei/backend/utils/AttackTypeUtils.java
+++ backend/src/main/java/net/estg/ei/backend/utils/AttackTypeUtils.java
@@ -8,13 +8,13 @@ import net.estg.ei.backend.enums.AttackType;
 
 public class AttackTypeUtils {
     private static ArrayList<AttackType> attackTypeDoubleListMapping = new ArrayList<>(
-        Arrays.asList( AttackType.APACHE_KILLER, AttackType.RUDY, AttackType.SLOW_READ,
+        Arrays.asList(AttackType.APACHE_KILLER, AttackType.RUDY, AttackType.SLOW_READ,
         AttackType.SLOW_LORIS, AttackType.ARP_SPOOFING, AttackType.CAM_OVERFLOW,
         AttackType.MQTT_MALARIA, AttackType.NETWORK_SCAN)
     );
 
     public static AttackType getAttackType(List<Double> predictions) {
-        for (int i = 0; i < predictions.size(); i++) {
+        for (int i = 1; i < predictions.size(); i++) {
             if (predictions.get(i) == 1.0) return attackTypeDoubleListMapping.get(i); 
         }
         
