ebee9cfd9100efac2cc3d0b28a9888ac7bad0121
Dora Mendes <97027733+DoraMendes@users.noreply.github.com>
2024-07-12 22:25:32 +0100

    Update settings.ts

--- frontend/src/settings.ts
+++ frontend/src/settings.ts
@@ -1,2 +1,2 @@
 export const API_URL = 'https://backend.mendes.ltd/api';
-export const WS_URL = 'ws://ec2-18-222-183-75.us-east-2.compute.amazonaws.com:8080/ws';
+export const WS_URL = 'wss://backend.mendes.ltd/ws';
