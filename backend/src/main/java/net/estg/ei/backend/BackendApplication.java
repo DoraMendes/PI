30ea6f20c7e0789261e64c0318216c041d9ee6ae
Dora Mendes <doramendes98@hotmail.com>
2024-07-12 05:53:46 +0100

    try enable cors

--- backend/src/main/java/net/estg/ei/backend/BackendApplication.java
+++ backend/src/main/java/net/estg/ei/backend/BackendApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.Arrays;
 
 @SpringBootApplication
+@CrossOrigin(origins = "http://localhost:3000")
 public class BackendApplication {
 
 	public static void main(String[] args) {
